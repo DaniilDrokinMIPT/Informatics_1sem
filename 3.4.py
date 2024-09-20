cba8709a72408cb527b318f6ec4a372dd37d1fe0
DaniilDrokinMIPT <drokin.dv@phystech.edu>
2024-09-20 18:26:07 +0300

    Add files via upload

--- 3.4.py
+++ 3.4.py
@@ -1,6 +1,12 @@
 n = int(input())
 symb = input()
-for i in range(n // 2 + 2):
-    print(symb * i)
-for i in range(n // 2):
-    print((n // 2 - i) * symb)
+if n % 2 == 0:
+    for i in range(n // 2 + 1):
+        print(symb * i)
+    for i in range(n // 2):
+        print(symb * (n // 2 - i))
+else:
+    for i in range(n // 2 + 2):
+        print(symb * i)
+    for i in range(n // 2):
+        print(symb * (n // 2 - i))
